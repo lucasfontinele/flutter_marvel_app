89b404831fe47b58f3acc106ff5fcb00343777e7
lucasfontinele <contato@fontinele.dev>
2025-06-17 16:16:52 -0300

    fix: :bug: heroes

--- lib/Scenes/Home/home_view.dart
+++ lib/Scenes/Home/home_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_marvel_app/components/Header/header_view.dart';
-import 'package:flutter_marvel_app/components/HeroCard/hero_card_view.dart';
 import 'package:flutter_marvel_app/components/ItemsList/items_list_view.dart';
 import 'package:flutter_marvel_app/models/hero_model.dart';
 import 'package:flutter_marvel_app/repositories/HeroesRepository.dart';
 import 'package:flutter_marvel_app/scenes/Error/error_factory.dart';
-import 'package:flutter_marvel_app/scenes/HeroDetail/hero_detail_view.dart';
 import 'package:flutter_marvel_app/scenes/Loading/loading_factory.dart';
 import 'package:flutter_marvel_app/Services/api_service.dart';
 import 'package:flutter_marvel_app/components/BottomNavigationBar/bottom_navigation_bar.dart';
@@ -85,65 +83,117 @@ class HomeViewState extends State<HomeView> {
         ),
       ),
       bottomNavigationBar: const BottomBar(),
-      body: RefreshIndicator(
-          child: Container(
-            padding: const EdgeInsets.only(left: 24, top: 24),
-            child: Stack(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 24, top: 24),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Bem-vindo ao Heroes",
-                      style: TextStyle(
-                        color: Color(0xFFB7B7C8),
-                        fontSize: 14,
-                      ),
-                    ),
-                    const Text(
-                      "Escolha o seu\npersonagem",
-                      style: TextStyle(
-                        color: Color(0xFF313140),
-                        fontSize: 32,
-                        fontFamily: "Gilroy-Heavy",
-                      ),
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 48),
-                    ItemsListView<HeroModel>(
-                      label: "Personagens Marvel",
-                      // items: marvelCharacters,
-                      items: [
-                        HeroModel(id: 1, name: "Teste", description: "Description", image: "https://static.wikia.nocookie.net/anicrossbr/images/3/38/Ichigo_kurosaki_render_by_stella1994x-d7sx64g.png/revision/latest?cb=20151022032048&path-prefix=pt-br", mainCharacteristics: "teste")
-                      ],
-                      itemBuilder: (context, character) {
-                        return HeroCard(
-                          heroName: character.name,
-                          imagePath: character.image,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HeroDetailView(
-                                  heroName: character.name,
-                                  realName: character.name,
-                                  imageUrl: character.image,
-                                  description: character.description,
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ],
+                const Text(
+                  "Bem-vindo ao Heroes",
+                  style: TextStyle(
+                    color: Color(0xFFB7B7C8),
+                    fontSize: 14,
+                  ),
+                ),
+                const Text(
+                  "Escolha o seu\npersonagem",
+                  style: TextStyle(
+                    color: Color(0xFF313140),
+                    fontSize: 32,
+                    fontFamily: "Gilroy-Heavy",
+                  ),
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
+
+                const SizedBox(height: 48),
+
+                ItemsListView<HeroModel>(
+                  label: "Personagens Marvel",
+                  items: marvelCharacters,
+                  itemBuilder: (context, character) {
+                    return Text(character.name);
+                    // return HeroCard(
+                    //   heroName: character.name,
+                    //   imagePath: character.image,
+                    //   onTap: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => HeroDetailView(
+                    //           heroName: character.name,
+                    //           realName: character.name,
+                    //           imageUrl: character.image,
+                    //           description: character.description,
+                    //         ),
+                    //       ),
+                    //     );
+                    //   },
+                    // );
+                  },
+                ),
+
+                const SizedBox(height: 12),
+
+                ItemsListView<HeroModel>(
+                  label: "Personagens DC",
+                  items: dcCharacters,
+                  itemBuilder: (context, character) {
+                    return Text(character.name);
+                    // return HeroCard(
+                    //   heroName: character.name,
+                    //   imagePath: character.image,
+                    //   onTap: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => HeroDetailView(
+                    //           heroName: character.name,
+                    //           realName: character.name,
+                    //           imageUrl: character.image,
+                    //           description: character.description,
+                    //         ),
+                    //       ),
+                    //     );
+                    //   },
+                    // );
+                  },
+                ),
+
+                const SizedBox(height: 12),
+
+                ItemsListView<HeroModel>(
+                  label: "Personagens Anime",
+                  items: animeCharacters,
+                  itemBuilder: (context, character) {
+                    return Text(character.name);
+                    // return HeroCard(
+                    //   heroName: character.name,
+                    //   imagePath: character.image,
+                    //   onTap: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => HeroDetailView(
+                    //           heroName: character.name,
+                    //           realName: character.name,
+                    //           imageUrl: character.image,
+                    //           description: character.description,
+                    //         ),
+                    //       ),
+                    //     );
+                    //   },
+                    // );
+                  },
                 ),
               ],
             ),
-          ),
-          onRefresh: () async {}),
+          ],
+        ),
+      ),
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: true,
     );
